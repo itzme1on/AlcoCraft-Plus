c42f0eb48b056e5e202d923ecd5dc9eedffe0686
Marty Mitchell <martyandmojo@icloud.com>
2023-06-08 13:51:01 -0700

    fix: JEI keg usage window not displaying

--- src/main/java/me/itzme1on/alcocraftplus/integrations/JEIPlugin.java
+++ src/main/java/me/itzme1on/alcocraftplus/integrations/JEIPlugin.java
@@ -1,13 +1,16 @@
 package me.itzme1on.alcocraftplus.integrations;
 
+import me.itzme1on.alcocraftplus.blocks.AlcoBlocks;
 import me.itzme1on.alcocraftplus.recipes.KegRecipe;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeManager;
 
 import java.util.List;
@@ -31,5 +34,11 @@ public class JEIPlugin implements IModPlugin {
         List<KegRecipe> recipes = manager.getAllRecipesFor(KegRecipe.Type.INSTANCE);
 
         registration.addRecipes(new RecipeType<>(KegCategory.UID, KegRecipe.class), recipes);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(AlcoBlocks.KEG.get()), new RecipeType<>(KegCategory.UID,
+                KegRecipe.class));
     }
 }
