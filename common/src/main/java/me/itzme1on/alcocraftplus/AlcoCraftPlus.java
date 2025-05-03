462bb06ba5362192090eb71157cd931e0131ade7
SexyLexi <audiosurfer2013@gmail.com>
2025-05-03 17:13:36 +0200

    Update AlcoCraftPlus.java

--- common/src/main/java/me/itzme1on/alcocraftplus/AlcoCraftPlus.java
+++ common/src/main/java/me/itzme1on/alcocraftplus/AlcoCraftPlus.java
@@ -3,16 +3,14 @@ package me.itzme1on.alcocraftplus;
 import dev.architectury.event.events.common.LifecycleEvent;
 import me.itzme1on.alcocraftplus.core.events.CommonEvents;
 import me.itzme1on.alcocraftplus.core.registries.*;
-import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class AlcoCraftPlus implements ModInitializer{
+public class AlcoCraftPlus {
     public static final String MOD_ID = "alcocraftplus";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-   @Override
-   public void onInitialize() {
+   public static void initializeCommon() {
        LOGGER.info("Initializing AlcoCraftPlus...");
 
         ScreenHandlerRegistry.register();
